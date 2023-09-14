a73c7e3ddab0c21897b5ef19a60b165dfdf5a130
Max Veerhoek <lmg@veerhoek.eu>
2023-09-14 11:56:09 +0200

    Add Exercise 1

--- app/Lab1/Exercise1.hs
+++ app/Lab1/Exercise1.hs
@@ -10,11 +10,30 @@ import Test.QuickCheck.Monadic
 import Test.QuickCheck.State
 
 -- Exercise 1
+-- A generator which only allows for natural numbers to be generates
+genPositiveNumbers :: Gen Integer
+genPositiveNumbers = abs <$> (arbitrary :: Gen Integer) `suchThat` (>= 0)
+
 -- https://www.wikiwand.com/en/Factorial
 factorial :: Integer -> Integer 
-factorial 0 = 1 
-factorial n = n * factorial (n-1)
+factorial n | n == 0 = 1
+            | n >= 0 = n * factorial (n - 1)
+            | otherwise = error "n is not allowed to be negative"
+
+isDivisableByX :: Integer -> Integer -> Bool 
+isDivisableByX n x = n `mod` x == 0
+
+prop_divisableBySmallerThanN :: Integer -> Bool 
+prop_divisableBySmallerThanN n = all (isDivisableByX (factorial n)) [1..n]
+
+prop_factorialBase :: Bool 
+prop_factorialBase = factorial 0 == 1
+
+prop_factorialIncrement :: Integer -> Bool 
+prop_factorialIncrement n = factorial n * (n+1) == factorial (n+1)
 
 main :: IO ()
 main = do
-  print $ factorial 10
+  quickCheck prop_factorialBase
+  quickCheck prop_divisableBySmallerThanN
+  quickCheck (forAll genPositiveNumbers prop_factorialIncrement)
