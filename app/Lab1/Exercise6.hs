df49676ccafe0bb19eb1ba7b2a87d6101983805a
Max Veerhoek <lmg@veerhoek.eu>
2023-09-21 13:02:41 +0200

    Voor Niels <3

--- app/Lab1/Exercise6.hs
+++ app/Lab1/Exercise6.hs
@@ -8,11 +8,37 @@ import System.Random
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
 import Test.QuickCheck.State
+import Lecture3
 
 -- Exercise 6
 -- Converting from first-order logic
 
+-- Example 1: Negation of Disjunction
+example1 :: Form
+example1 = Neg (Dsj [Prop 1, Prop 2])
+
+-- Example 2: Implication
+example2 :: Form
+example2 = Impl (Prop 1) (Prop 2)
+
+-- Example 3: Nested Equivalence
+example3 :: Form
+example3 = Equiv (Equiv (Prop 1) (Prop 2)) (Prop 3)
+
+-- Example 4: Negation of a Complex Formula
+example4 :: Form
+example4 = Neg (Impl (Prop 1) (Cnj [Prop 2, Prop 3]))
+
+-- Example 5: Combination of Connectives
+example5 :: Form
+example5 = Impl (Cnj [Prop 1, Dsj [Prop 2, Prop 3]]) (Prop 4)
+
+
 -- Convert to negation normal form 
+cnf :: Form -> Form
+cnf = nnf . arrowfree
+
+
 -- Eliminate implications
 -- Repeatedly replace P -> Q with ~P v Q
 
@@ -20,4 +46,6 @@ import Test.QuickCheck.State
 
 main :: IO ()
 main = do
-  print "Hello World"
+  putStrLn $ concatMap (\e -> show e ++ " \n" ) [example1, example2, example3, example4, example5]
+  putStrLn $ concatMap (\e -> show (cnf e) ++ " \n" ) [example1, example2, example3, example4, example5]
+  print $ cnf form2
--- app/Lab1/Exercise7.hs
+++ app/Lab1/Exercise7.hs
@@ -1,16 +1,81 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Lab1.Exercise7 where
 
-import Control.Monad
 import Data.Char
 import Data.List
-import GHC.Float
-import System.Random
+import Lecture3
+import SetOrd
 import Test.QuickCheck
-import Test.QuickCheck.Monadic
 import Test.QuickCheck.State
 
 -- Exercise 7
 
+-- A function which determines the amount of elements in a Set
+sizeSet :: Set a -> Int
+sizeSet (Set xs) = length xs
+
+-- A function which determines the union of a list of Sets
+unionSets :: Ord a => [Set a] -> Set a
+unionSets = foldr unionSet emptySet
+
+-- Counts all the subformulas of a formula
+nsub :: Form -> Int
+nsub form = sizeSet $ nsub' emptySet form
+  where
+    nsub' :: Set Form -> Form -> Set Form
+    nsub' subforms formula = insertSet formula $ 
+      case formula of
+        Prop _ -> subforms
+        Neg f -> nsub' subforms f
+        Cnj fs -> recurse fs
+        Dsj fs -> recurse fs
+        Impl f1 f2 -> recurse [f1, f2]
+        Equiv f1 f2 -> recurse [f1, f2]
+        where
+          recurse = unionSets . map (nsub' subforms)
+
+arbitraryPositiveInt :: Int -> Gen Int
+arbitraryPositiveInt n = choose (1, n :: Int)
+
+instance Arbitrary Form where
+  arbitrary :: Gen Form
+  arbitrary = sized $ \n -> do
+    positiveIntegers <- vectorOf (n + 1) $ arbitraryPositiveInt (n + 1)
+    genForm positiveIntegers n
+
+genForm :: [Int] -> Int -> Gen Form
+genForm positiveIntegers 0 = Prop <$> elements positiveIntegers
+genForm positiveIntegers n =
+  oneof
+    [ Prop <$> elements positiveIntegers,
+      Neg <$> arbitrary,
+      Cnj <$> formList,
+      Dsj <$> formList,
+      Impl <$> subForm <*> subForm,
+      Equiv <$> subForm <*> subForm
+    ]
+  where
+    -- The size parameter is halved at each level of recusion to limit the depth of recursion.
+    -- TO-DO: make n based on the amount of subformulas
+    subForm = genForm positiveIntegers (n `div` 2)
+    -- We assume that the amount of subformulas for a conjunction and disjunction is 2
+    formList = vectorOf 2 subForm
+
+-- Time Spent on generator: 30 minutes
+
+-- A fomula based on form3, but with a formula added to one of the conjunctions
+-- It should return the same amount of subformulas as form3 + 1
+form4 :: Form
+form4 = Impl (Cnj [Impl p q, Impl q r, Impl r p]) (Impl p r)
+
 main :: IO ()
 main = do
-  print "Hello World"
+  -- Generate and print a small random formula of type Form
+  formula <- generate $ resize 2 arbitrary :: IO Form
+  print formula
+  print $ nsub formula
+  print $ nsub form1
+  print $ nsub form2
+  print $ nsub form3
+  print $ nsub form4
