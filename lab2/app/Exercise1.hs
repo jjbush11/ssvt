a6f5e7de3dbefefe0a7a1dc08c3d3071bfb71add
Max Veerhoek <lmg@veerhoek.eu>
2023-09-27 14:20:12 +0200

    Add exercise 1 and an unfinished exercise 6

--- lab2/app/Exercise1.hs
+++ lab2/app/Exercise1.hs
@@ -1,11 +1,33 @@
 module Exercise1 where
 
-import Test.QuickCheck
 import LTS
+import Test.QuickCheck
 
+-- The validity of an LTS can be checked, by checking the following properties:
+-- 1. There are no empty states in Q
+-- 2. There are no empty labels
+-- 3. All transitions are valid and contain valid labels
+-- 4. Check if the initial state is valid.
 validateLTS :: IOLTS -> Bool
-validateLTS = undefined
+validateLTS (states, inputLabels, outputLabels, transitions, initialState) =
+  validateStates states
+    && validateTransitions transitions
+  where
+    validateStates :: [State] -> Bool
+    validateStates = not . null
+
+    validateTransitions :: [LabeledTransition] -> Bool
+    validateTransitions = all isValidTransition
+
+    isValidTransition :: LabeledTransition -> Bool
+    isValidTransition (q, l, q') = q `elem` states && (l `elem` inputLabels || l `elem` outputLabels) && q' `elem` states
+
+    validateInitialState :: State -> [State] -> Bool
+    validateInitialState initialState states = initialState `elem` states
+
+    deathLocks = undefined
+    isDeadlock (q, l, q') states = q `notElem` states 
 
 main :: IO ()
-main = do 
-  print "Hello World"
+main = do
+  print $ validateLTS counterModel
--- lab2/app/Exercise6.hs
+++ lab2/app/Exercise6.hs
@@ -2,10 +2,31 @@ module Exercise6 where
 
 import Test.QuickCheck
 import LTS
+import Data.GraphViz
+import Data.GraphViz.Attributes.Complete
+import Data.GraphViz.Printing
+import Data.Text.Lazy.IO as TIO 
+import qualified Data.Text.Lazy as TL
 
-visualizeLTS :: IOLTS -> String
-visualizeLTS = undefined
+convertLTS :: ([Integer], [(Integer, String, Integer)]) -> Data.GraphViz.DotGraph Integer
+convertLTS (states, transitions) =
+  let nodes = convertNodesToFormat states
+      edges = map convertTransition transitions
+  in graphElemsToDot params (convertNodesToFormat states) edges
+  where
+    params = nonClusteredParams { fmtNode = \(_,label) -> [textLabel label] }
+    convertNodesToFormat = map (\state -> (state, TL.pack $ show state))
+    convertTransition (from, label, to) = (from, to, label)
+
+ltsToDotGraph :: IOLTS -> DotGraph Integer
+ltsToDotGraph (states, _, _, transitions, _) =
+  convertLTS (states, transitions)
+
+-- A function which prints a graph to the terminal
+printGraph :: DotGraph Integer -> IO ()
+printGraph = TIO.putStrLn . printDotGraph
+
 
 main :: IO ()
-main = do 
-  print "Hello World"
+main = do
+  printGraph $ ltsToDotGraph coffeeImpl1
