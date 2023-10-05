51347f29760d2c9f8f8562c4a074040876624d80
Max Veerhoek <lmg@veerhoek.eu>
2023-10-05 11:05:29 +0200

    Propabably fix Exercise 2
    Co-authored-by: Niels Arts <nielsarts@users.noreply.github.com>
    Co-authored-by: Jasper van den Barg <jaspervandenbarg@users.noreply.github.com>

--- lab3/app/Exercise2.hs
+++ lab3/app/Exercise2.hs
@@ -5,6 +5,7 @@ import Control.Monad
 import Mutation
 import MultiplicationTable
 import Exercise1
+import FitSpec (testFitSpec)
 
 -- Exercise 2
 -- Where the first argument is the number of mutants (4000 in the FitSpec example), 
@@ -16,23 +17,48 @@ import Exercise1
 countSurvivors :: Integer -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> [[Integer] -> Gen [Integer]] -> IO Integer
 countSurvivors nMutants properties fut mutators = do
     -- Generate a list of random inputs for the mutants
+    -- The same inputs are used for each mutator
+    -- By doing this, we can compare the effectiveness of the mutators
     inputs <- generate $ vectorOf (fromIntegral nMutants) arbitrary
 
-    -- For each input, generate mutants using each mutator and test against properties
-    results <- forM inputs $ \input -> do
-        mutantsResults <- forM mutators $ \mutator -> do
+    -- For each mutator, generate mutants using the generated inputs and test against properties
+    results <- forM mutators $ \mutator -> do
+        forM inputs $ \input -> do
             result <- generate (mutate' mutator properties fut input)
-            return (not $ or result)  -- True if the mutant survived all properties
-        return (or mutantsResults)  -- True if the mutant survived any mutator
+            -- -- If one of the properties fails, return False
+            -- -- If the result is empty, return False as well 
+            -- return $ any (== False) result
+            -- If one of the properties fails, return False
+            -- If the result is empty, return False as well 
+            return $ and result
 
-    -- Count the number of True values (survivors)
-    return $ fromIntegral $ length $ filter id results
+    return $ fromIntegral $ length $ filter id $ concat results
+
+countSurvivors' :: Integer -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> [[Integer] -> Gen [Integer]] -> IO Integer
+countSurvivors' nMutants properties fut mutators = do
+    -- Generate a list of random inputs for the mutants
+    -- The same inputs are used for each mutator
+    -- By doing this, we can compare the effectiveness of the mutators
+    inputs <- generate $ vectorOf (fromIntegral nMutants) arbitrary
+
+    -- For each mutator, generate mutants using the generated inputs and test against properties
+    results <- forM mutators $ \mutator -> do
+        forM inputs $ \input -> do
+            result <- generate (mutate' mutator properties fut input)
+            -- -- If one of the properties fails, return False
+            -- -- If the result is empty, return False as well 
+            -- return $ any (== False) result
+            -- If one of the properties fails, return False
+            -- If the result is empty, return False as well 
+            return $ and result
+
+    return $ fromIntegral $ length $ filter id $ concat results
 
 -- Document the effect of which mutations are used and which properties are used on the number of survivors.
 
-
 main :: IO ()
 main = do
+    -- Experimenting with the mutate' function
     let fut = multiplicationTable
     let properties = multiplicationTableProps
     let mutator = negateElements
@@ -41,6 +67,9 @@ main = do
     print results  -- This will print a list of Bool values
 
     let nMutants = 4000
-    let mutators = [sortList, duplicateElements]
+    let mutators = [sortList, duplicateElements, singleElementList, negateElements]
     survivors <- countSurvivors nMutants properties fut mutators
     print survivors
+
+    -- Run fitspec for comparison to our own implementation
+    -- testFitSpec 
