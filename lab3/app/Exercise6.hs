d6ebdaf998fbd935a79e2b6fbf3bd5081df78d69
Thom Pheijffer <tpheijffer@icloud.com>
2023-10-10 21:49:30 +0200

    Implemented start of Exercise 6 without analysis of exercise 3 and 5

--- lab3/app/Exercise6.hs
+++ lab3/app/Exercise6.hs
@@ -1,5 +1,12 @@
 module Exercise6 where
 
+import Control.Monad
+import Data.List (subsequences, transpose)
+import Exercise1
+import Exercise2
+import FitSpec (testFitSpec)
+import MultiplicationTable
+import Mutation
 import Test.QuickCheck
 
 -- Exercise 6 (Bonus)
@@ -8,6 +15,32 @@ import Test.QuickCheck
 visualizeResults :: [a -> Integer -> Bool] -> (Integer -> a) -> Integer -> String
 visualizeResults = undefined
 
+report :: [[Integer] -> Gen [Integer]] -> Integer -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> IO ()
+report mutators nMutants properties fut = do
+  -- We first execute the executeMutation function to get the raw results
+  res <- executeMutation mutators nMutants properties fut
+
+  -- We then apply the computeAnalysis function to get a PropertyAnalysis record.
+  let transposedResults = transposeRawResults res
+  let raw = map (length . snd) transposedResults
+  let indices = map fst $ transposeRawResults res
+  let analysis = computeAnalysis transposedResults
+
+  -- Print report based on the calculated analysis
+  print $ "The total number of mutants is: " ++ show (totalMutants analysis)
+  print $ show (nKilled analysis) ++ " mutants killed in total"
+  print $ show (nSurvivors analysis) ++ " survivors (" ++ show (score analysis) ++ "% killed)"
+
 main :: IO ()
 main = do
-  print "Hello World"
+  -- Define the inputs for the report function
+  let fut = multiplicationTable
+  let properties = multiplicationTableProps
+  let mutator = negateElements
+  let nMutants = 4000
+  let mutators = [sortList, duplicateElements, singleElementList, negateElements, removeElements, zeroElements, shuffleElements, powElements, multiplyByArbitrary, addOneToElements] -- , anyList, emptyList]
+
+  -- Print a report
+  report mutators nMutants properties fut
+
+-- Indication of time spent: 1 hour
