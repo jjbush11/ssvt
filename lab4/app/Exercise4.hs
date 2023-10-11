f3dd931c0ce40df6e686ea9d14cf1a544fc16604
Max Veerhoek <lmg@veerhoek.eu>
2023-10-11 14:57:31 +0200

    Add Exercise 4

--- lab4/app/Exercise4.hs
+++ lab4/app/Exercise4.hs
@@ -5,7 +5,10 @@ import SetOrd
 import Exercise3
 
 isSerial :: Eq a => [a] -> Rel a -> Bool
-isSerial = undefined
+isSerial domain rel = all (hasRelation domain rel) domain  
+  where
+    hasRelation :: Eq a => [a] -> Rel a -> a -> Bool
+    hasRelation domain rel x = any (\y -> (x,y) `elem` rel) domain  
 
 main :: IO ()
 main = do 
