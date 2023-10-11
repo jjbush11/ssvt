14850ecbbc2a26e2b37482d73c77a6b911267d69
Max Veerhoek <lmg@veerhoek.eu>
2023-10-11 13:06:48 +0200

    Add implementations Exercise 2
    Co-authored-by: Jasper van den Barg <jaspervandenbarg@users.noreply.github.com>

--- lab4/app/Exercise2.hs
+++ lab4/app/Exercise2.hs
@@ -2,17 +2,20 @@ module Exercise2 where
 
 import Test.QuickCheck
 import SetOrd
+import Data.List
 
-setIntersection :: Ord a => Set a -> Set a -> Set a 
-setIntersection = undefined
+-- The intersection means all the elements that are in both sets.
+setIntersection :: Ord a => Set a -> Set a -> Set a
+setIntersection (Set a) (Set b) = Set [x | x <- a, x `elem` b]
 
-setUnion :: Ord a => Set a -> Set a -> Set a 
-setUnion = undefined
+-- The union means all the elements that are in either set.
+setUnion :: Ord a => Set a -> Set a -> Set a
+setUnion (Set a) (Set b) = Set $ nub (a ++ b)
 
+-- The difference means all the elements that are in the first set, but not in the second.
 setDifference :: Ord a => Set a -> Set a -> Set a
-setDifference = undefined
-
+setDifference (Set a) (Set b) = Set [x | x <- a, x `notElem` b]
 
 main :: IO ()
-main = do 
+main = do
   print "Hello World"
