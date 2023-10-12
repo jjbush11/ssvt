80788913a3742b72a0b03af35776112ab0cbb842
Max Veerhoek <lmg@veerhoek.eu>
2023-10-12 14:51:50 +0200

    Add a show instance for Statement in Bonus1

--- lab4/app/Bonus1.hs
+++ lab4/app/Bonus1.hs
@@ -1,9 +1,58 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Bonus1 where
 
+import Data.List (intercalate)
+import Lecture6
 import Test.QuickCheck
 
--- Missing the Statement data type from the Lecture notes. 
+instance Show Expr where
+  show :: Expr -> String
+  show e = case e of
+    I i -> show i
+    V v -> v
+    Add e1 e2 -> show e1 ++ " - " ++ show e2
+    Subtr e1 e2 -> show e1 ++ " - " ++ show e2
+    Mult e1 e2 -> show e1 ++ " * " ++ show e2
+
+instance Show Condition where
+  show :: Condition -> String
+  show c = case c of
+    Prp v -> v
+    Eq e1 e2 -> show e1 ++ " == " ++ show e2
+    Lt e1 e2 -> show e1 ++ " < " ++ show e2
+    Gt e1 e2 -> show e1 ++ " > " ++ show e2
+    Ng c -> "!(" ++ show c ++ ")"
+    Cj cs -> intercalate " && " (map show cs)
+    Dj cs -> intercalate " || " (map show cs)
+
+-- A function to add indentation
+indent :: String -> String
+indent = unlines . map ("  " ++) . lines
+
+-- A show instance for the Statement data type.
+instance Show Statement where
+  show :: Statement -> String
+  show s = case s of
+    Ass v e -> v ++ " = " ++ show e ++ ";"
+    Cond c s1 s2 ->
+      "if ("
+        ++ show c
+        ++ ") {\n"
+        ++ indent (show s1)
+        ++ "}\nelse {\n"
+        ++ indent (show s2)
+        ++ "}"
+    Seq stmts ->
+      intercalate "" (map (indent . show) stmts)
+    While c s ->
+      "while ("
+        ++ show c
+        ++ ") {\n"
+        ++ indent (show s)
+        ++ "}"
 
 main :: IO ()
 main = do
-  print "Hello World"
+  print fib
+  print fib'
--- lab4/app/Exercise6.hs
+++ lab4/app/Exercise6.hs
@@ -4,8 +4,6 @@ import Test.QuickCheck
 import Exercise4
 import Exercise5
 
-
-
 main :: IO ()
 main = do 
   print "Hello World"
