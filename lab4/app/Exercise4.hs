585cd7167708f61181d4a99e162e56278ddf400c
Jasper van den Barg <73914053+jaspervandenbarg@users.noreply.github.com>
2023-10-18 15:13:02 +0200

    quickcheck properties for excersice 4 1.1

--- lab4/app/Exercise4.hs
+++ lab4/app/Exercise4.hs
@@ -2,12 +2,33 @@ module Exercise4 where
 
 import Exercise3
 import Test.QuickCheck
+import Data.List
 
 isSerial :: (Eq a) => [a] -> Rel a -> Bool
 isSerial domain rel = all (hasRelation domain rel) domain
   where
     hasRelation :: (Eq a) => [a] -> Rel a -> a -> Bool
     hasRelation domain rel x = any (\y -> (x, y) `elem` rel) domain
+
+prop_isSerial_returns_true_for_identity_relation :: NonEmptyList Int -> Property
+prop_isSerial_returns_true_for_identity_relation (NonEmpty domain) =
+  let rel = [(x, x) | x <- domain]
+  in isSerial domain rel === True
+
+prop_isSerial_returns_false_for_empty_relation :: NonEmptyList Int -> Property
+prop_isSerial_returns_false_for_empty_relation (NonEmpty domain) =
+  let rel = []
+  in isSerial domain rel === False
+
+prop_isSerial_returns_false_for_incomplete_relation :: NonEmptyList Int -> Property
+prop_isSerial_returns_false_for_incomplete_relation (NonEmpty domain) =
+  let rel = [(x, y) | x <- domain, y <- domain, x < y]
+  in isSerial domain rel === False
+
+prop_isSerial_returns_false_for_asymmetric_relation :: NonEmptyList Int -> Property
+prop_isSerial_returns_false_for_asymmetric_relation (NonEmpty domain) =
+  let rel = [(x, y) | x <- domain, y <- domain, x < y]
+  in isSerial domain rel === (not $ all (\(x, y) -> (y, x) `notElem` rel) rel)
 
 modularRelation :: Rel Int
 modularRelation = [(x, y) | x <- [0 ..], y <- [0 ..], n <- [1 ..], x == y `mod` n]
@@ -26,6 +47,12 @@ modularRelationForN n = [(x, y) | x <- [0 .. 10], y <- [0 .. 10], x `mod` n == y
 
 main :: IO ()
 main = do
+
+  quickCheck prop_isSerial_returns_true_for_identity_relation
+  quickCheck prop_isSerial_returns_false_for_empty_relation
+  quickCheck prop_isSerial_returns_false_for_incomplete_relation
+  quickCheck prop_isSerial_returns_false_for_asymmetric_relation
+
   print $ take 10 modularRelation
   print $ take 30 $ modularRelationForN 2
 
